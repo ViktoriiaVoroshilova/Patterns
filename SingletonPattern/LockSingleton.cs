d2c50c86eac93cb56787df295a4105fdd300c7b1
Viktoriia Voroshilova <viktoriia_voroshilova@epam.com>
2022-08-31 15:33:04 +0400

    added singleton

---
+++ SingletonPattern/LockSingleton.cs
@@ -0,0 +1,27 @@
+﻿namespace SingletonPattern;
+
+internal class LockSingleton
+{
+    public string Date { get; private set; }
+
+    private LockSingleton() => Date = DateTime.Now.TimeOfDay.ToString();
+
+    public static LockSingleton Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                lock (_locker)
+                {
+                    if (_instance == null) _instance = new LockSingleton(); ;
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    private static LockSingleton? _instance;
+    private static readonly object _locker = new();
+}
